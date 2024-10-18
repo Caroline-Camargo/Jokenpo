3c420d7d27ad7be23e719f491cd58adb6448441f
Caroline Souza Camargo <carolinecamargo012@gmail.com>
2024-10-18 11:07:48 -0300

    Add PostMapping

--- backend/src/main/java/com/example/jokenpo/controllers/JokenpoGameController.java
+++ backend/src/main/java/com/example/jokenpo/controllers/JokenpoGameController.java
@@ -1,6 +1,8 @@
 package com.example.jokenpo.controllers;
 
+import com.example.jokenpo.dtos.JokenpoMatchRecordDto;
 import com.example.jokenpo.services.JokenpoGameService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -15,6 +17,20 @@ public class JokenpoGameController {
     @GetMapping("/jokenpo/play/{namePlayer1}/{player1option}")
     public ResponseEntity<String> playGame(@PathVariable String namePlayer1, @PathVariable String player1option) {
         String result = jokenpoGameService.playGame(namePlayer1, player1option);
+        if (result.equals("Invalid option!")) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Invalid option! Please choose rock, paper, or scissors.");
+        }
+        return ResponseEntity.status(HttpStatus.OK).body(result);
+    }
+
+    @PostMapping("/jokenpo/play")
+    public ResponseEntity<String> playGamePost(@RequestBody JokenpoMatchRecordDto matchRecordDto) {
+        if (matchRecordDto.player1() == null || matchRecordDto.player1().isEmpty()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Player name field is required.");
+        } else if (matchRecordDto.choice1() == null || matchRecordDto.choice1().isEmpty()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Player choice field is required.");
+        }
+        String result = jokenpoGameService.playGame(matchRecordDto.player1(), matchRecordDto.choice1());
         if (result.equals("Invalid option!")) {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Invalid option! Please choose rock, paper, or scissors.");
         }
